75212183021ae80af4d4b9935ee064b5e7d32ff0
TeletobyAU <tal03.mc@gmail.com>
2021-02-02 12:05:27 +0100

    initial

--- SuffixArrayGenerator.py
+++ SuffixArrayGenerator.py
@@ -10,23 +10,27 @@ def generateSuffixArray(inputString):
     for i in range (len(workingString)):
         suffixArray.append(workingString[i : len(workingString)] + workingString[0 : i])
 
+    if printResult:
+        print("\nSuffix array:")
+        print(suffixArray)
+
+    return suffixArray
+
+def generateBWT(suffixArray):
     sortedSuffixArray = suffixArray.copy()
     sortedSuffixArray.sort()
 
     bwt = ""
-    for i in range (len(sortedSuffixArray)):
+    for i in range(len(sortedSuffixArray)):
         item = sortedSuffixArray[i]
         bwt += item[-1]
 
     if printResult:
-        print("Suffix array:")
-        print(suffixArray)
-        print("Sorted suffix array:")
+        print("\nSorted suffix array:")
         print(sortedSuffixArray)
-        print("Burrows Wheeler transformation string:")
+        print("\nBurrows Wheeler transformation string:")
         print(bwt)
 
-    return suffixArray
-
 inp = input("Type string you wish to create suffix array from: \n")
-generateSuffixArray(inp)
+suffixArray = generateSuffixArray(inp)
+generateBWT(suffixArray)
