1a971fe1e1c49e4b724abb0d85a970093c5fda91
TeletobyAU <tal03.mc@gmail.com>
2021-03-22 15:20:17 +0100

    c table and exact search

--- SuffixArrayGenerator.py
+++ SuffixArrayGenerator.py
@@ -5,83 +5,150 @@ printResult = True
 
 import RandomRNAStringGenerator
 
-def generateSuffixArray(inputString):
-    workingString = inputString + '$'
 
-    suffixArray = []
-    for i in range (len(workingString)):
-        suffixArray.append(workingString[i : len(workingString)] + workingString[0 : i])
+
+def generateSuffixes(inputString):
+    suffixes = []
+    for i in range(len(inputString)):
+        suffixes.append(inputString[i: len(inputString)] + inputString[0: i])
 
     if printResult:
-        print("\nPivots:")
-        for s in suffixArray:
+        print("\nSuffixes:")
+        for s in suffixes:
             print(s)
+
+        print("\nSorted suffixes:")
+        for s in sorted(suffixes):
+            print(s)
+
+    return suffixes
+
+
+def generateSuffixArray(suffixes):
+    suffixArray = []
+    for s in sorted(suffixes):
+        suffixArray.append(suffixes.index(s))
+
+    if printResult:
+        print("\nSuffixarray:")
+        print(suffixArray)
 
     return suffixArray
 
-def generateBWT(suffixArray):
-    sortedSuffixArray = suffixArray.copy()
-    sortedSuffixArray.sort()
 
+def generateBWT(suffixes, suffixArray):
     bwt = ""
-    for i in range(len(sortedSuffixArray)):
-        item = sortedSuffixArray[i]
-        bwt += item[-1]
+    for i in range(len(suffixArray)):
+        bwt += suffixes[suffixArray[i]][-1]
 
     if printResult:
-        print("\nSorted suffix array:")
-        for s in sortedSuffixArray:
-            print(s)
         print("\nBurrows Wheeler transformation string:")
         print(bwt)
 
-def exactSearch(n, k):
-    print("\nWe are matching", k, "to", n + ":")
+    return bwt
 
-    matches = 0
+
+def exactSearch(n, k):
+    if printResult: print("\nWe are matching", k, "to", n + ":")
+
+    matches = []
     for i in range(len(n)):
         if n[i : i + len(k)] == k:
-            matches += 1
+            matches.append(i)
+            if printResult: print(k, "matched at index", i)
 
-            if printResult:
-                print(k, "matched at index", i)
-    if printResult:
-        print("Matches: ", matches)
+    if printResult: print("Number of matches: ", len(matches))
+
+    return matches
 
 
-def genCTable (input, alphabet):
-    if printResult:
-        print("\nThe C table:")
+def genCTable (n, alphabet):
+    if printResult: print("\nThe C table:")
 
     #Define alphabet if none given
-    input += '$'
     if alphabet == None:
         alphabet = []
-        for i in input:
-            if i not in alphabet:
-                alphabet.append(i)
+        for i in n:
+            if i not in alphabet: alphabet.append(i)
         alphabet.sort()
 
     output = []
     for i in alphabet:
         output.append(0)
-        for j in input:
-            if i > j:
-                output[alphabet.index(i)] += 1
+        for j in n:
+            if i > j: output[alphabet.index(i)] += 1
 
     if printResult:
-        for i in range(len(output)):
-            print(alphabet[i], ":", output[i])
+        for i in range(len(output)): print(alphabet[i], ":", output[i])
 
     return output
 
-#inp = input("Type string you wish to create suffix array from: \n")
-#inp = "mississippi"
-inp = RandomRNAStringGenerator.generateString(100)
-suffixArray = generateSuffixArray(inp)
-bwt = generateBWT(suffixArray)
-genCTable(inp, None)
 
-matchItem = "GCCGT"
+def genOTable(n, sa, BWT, alphabet):
+    if printResult: print("\nThe O table:")
 
-exactSearch(inp, matchItem)
+    #Define alphabet if none given
+    if alphabet == None:
+        alphabet = []
+        for i in n:
+            if i not in alphabet: alphabet.append(i)
+        alphabet.sort()
+
+    oTableSize = len(alphabet) #* len(sa) + 1 #* len(oTableSize)
+    oIndicesSize = len(sa) + 1 #* len(oIndicesSize)
+    if printResult: print("O table size:", oTableSize, "\nO indices size:", oIndicesSize)
+
+    oTable = []
+    oIndices = []
+
+    #First weird loop
+    #put lists of correct size into the o indices table
+    for i in range(oTableSize):
+        oIndices.append([])
+        for j in range(oIndicesSize):
+            oIndices[i].append(None)
+
+    for i in range(1, oTableSize):
+        oIndices[i][0] = 0
+
+    if printResult:
+        for i in oIndices:
+            print(i)
+
+
+
+def testMississippi():
+    inp = "mississippi$"
+    matchItem = "ss"
+
+    s = generateSuffixes(inp)
+    suffixArray = generateSuffixArray(s)
+    bwt = generateBWT(s, suffixArray)
+    genCTable(inp, None)
+    genOTable(inp, suffixArray, bwt, None)
+    exactSearch(inp, matchItem)
+
+
+def testRandomNucleotideString(nLen, kLen):
+    inp = RandomRNAStringGenerator.generateString(nLen) + "$"
+    matchItem = RandomRNAStringGenerator.generateString(kLen)
+
+    s = generateSuffixes(inp)
+    suffixArray = generateSuffixArray(s)
+    bwt = generateBWT(s, suffixArray)
+    genCTable(inp, None)
+    genOTable(inp, suffixArray, bwt, None)
+    exactSearch(inp, matchItem)
+
+
+def testGoogol():
+    n = "googol$"
+    s = generateSuffixes(n)
+    sa = generateSuffixArray(s)
+    bwt = generateBWT(s, sa)
+
+
+
+#testGoogol()
+testMississippi()
+#testRandomNucleotideString(100, 2)
